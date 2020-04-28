f3df6ee83d66406fd82e3293f53ac8774e44fbef
Alex Feltus <alex.feltus@gmail.com>
2020-04-28 17:05:26 -0400

    Create 01-Prepare-inputs.sh

---
+++ 01-Prepare-inputs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASEDIR=$(pwd)
+mkdir -p $BASEDIR/FastQ
+cp $BASEDIR/Templates/basedir.txt $BASEDIR/FastQ
+
+for i in `cat SRAList.txt` ; do cat $BASEDIR/Templates/FASTQ-DUMP.template | \
+sed s/SRAID/$i/g > $BASEDIR/FastQ/$i.FastQDump.pbs ; done
+
+cd $BASEDIR/FastQ
+
+for i in *Dump.pbs ; do ccqsub -o $BASEDIR/Logs/$i.SRA.out \
+-e $BASEDIR/Logs/$i.SRA.err ./$i ; done
+
+mkdir -p PBS
+mv *pbs PBS
