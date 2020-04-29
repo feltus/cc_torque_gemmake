8f77e4f44680bb5030923700ea9b405bb29fd264
Alex Feltus <alex.feltus@gmail.com>
2020-04-29 00:05:17 -0400

    Update 00_C_IndexGenome.sh

--- 00_C_IndexGenome.sh
+++ 00_C_IndexGenome.sh
@@ -3,7 +3,7 @@
 #Change the reference genome index root name as needed
 REF_PREFIX=TAIR10
 
-cat ./Templates/Index.template | sed s/REF_PREFIX/$REF_PREFIX/g >> \
+cat ./Templates/INDEX.template | sed s/REF_PREFIX/$REF_PREFIX/g >> \
 ./Reference/RefIndex.pbs
 
 cd ./Reference/
