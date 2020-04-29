8cd2cad5d4ca1017cefc4c96c060b465e8afe504
Alex Feltus <alex.feltus@gmail.com>
2020-04-29 00:01:27 -0400

    Update 00_B_DownloadReference.sh

--- 00_B_DownloadReference.sh
+++ 00_B_DownloadReference.sh
@@ -3,7 +3,7 @@
 #Change to the working directory
 cd /mnt/orangefs/cc_torque_gemmake/Reference
 
-#Downlad the reference genome (FASTA format) and associated gene coordinate file (GFF3 format)
+#Download the reference genome (FASTA format) and associated gene coordinate file (GFF3 format)
 wget ftp://ftp.ensemblgenomes.org/pub/release-35/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
 wget ftp://ftp.ensemblgenomes.org/pub/release-35/plants/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.35.gff3.gz
 gunzip *.gz
