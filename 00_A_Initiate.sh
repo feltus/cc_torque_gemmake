9989a1a314dc94dd7dd86e59ec7b5af5a912ea89
Alex Feltus <alex.feltus@gmail.com>
2020-04-30 19:14:58 -0400

    Update 00_A_Initiate.sh

--- 00_A_Initiate.sh
+++ 00_A_Initiate.sh
@@ -14,6 +14,7 @@ wget ftp://ftp.ccb.jhu.edu/pub/infphilo/hisat2/downloads/hisat2-2.1.0-Linux_x86_
 wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2
 
 unzip Trimmomatic-0.39.zip
+tar xfz sratoolkit.2.9.0-centos_linux64.tar.gz
 tar xfz stringtie-2.1.2.Linux_x86_64.tar.gz
 unzip hisat2-2.1.0-Linux_x86_64.zip
 bunzip2 samtools-1.10.tar.bz2
